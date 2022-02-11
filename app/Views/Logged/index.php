5c0be6b334dade643c69adf477426ca2e1c7530b
Daniel Carvalho <danielcarvalhodev@gmail.com>
2022-02-11 19:27:11 -0300

    Melhoria no visual

--- app/Views/Logged/index.php
+++ app/Views/Logged/index.php
@@ -1,4 +1,4 @@
-<hr>
+<hr class="separator">
 <?php if(isset($_SESSION['msg'])): ?>
     <div class="text-center alert alert-warning alert-dismissible fade show">
         <strong><?= $_SESSION['msg'] ?></strong>
@@ -10,31 +10,42 @@
 
 <div class="text-center p-4">
     <h1>Bem-Vindo</h1>
-    <a class="btn btn-primary" href="/logout">Sair</a>
-    
+   
     <div class="p-5">
-        <h2>Produtos</h2>
+        <h2 class="text-secondary">Produtos</h2>
+        <hr class="separator">
         <div class="row">
-            <div class="col-sm-4 p-1">
-                <a class="btn btn-primary" href="/newProductForm">Novo Produto</a>
+            <div class="col-lg-4 p-1">
+                <a class="btn btn-primary btn-size" href="/newProductForm">Novo Produto</a>
             </div>
-            <div class="col-sm-4 p-1">
-                <a class="btn btn-primary" href="/listProducts">Listar Produtos</a>
+            <div class="col-lg-4 p-1">
+                <a class="btn btn-primary btn-size" href="/listProducts">Listar Produtos</a>
             </div>
-            <div class="col-sm-4 p-1">
-                <a class="btn btn-primary" href="/report">Relatório de Releavância de Produtos</a>
+            <div class="col-lg-4 p-1">
+                <a class="btn btn-primary btn-size" href="/report">Relatório</a>
             </div>
         </div>
     </div>
  
     <div class="p-5">
-        <h2>Tags</h2>
+        <h2 class="text-secondary">Tags</h2>
+        <hr class="separator">
         <div class="row">
-            <div class="col-sm-6 p-1">
-                <a class="btn btn-primary" href="/newTagForm">Nova Tag</a>
+            <div class="col-lg-6 p-1">
+                <a class="btn btn-primary btn-size" href="/newTagForm">Nova Tag</a>
             </div>
-            <div class="col-sm-6 p-1">
-                <a class="btn btn-primary" href="/listTags">Listar Tags</a>
+            <div class="col-lg-6 p-1">
+                <a class="btn btn-primary btn-size" href="/listTags">Listar Tags</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="p-5">
+        <h2 class="text-secondary">Usuário</h2>
+        <hr class="separator">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <a class="btn btn-primary btn-size" href="/logout">Sair</a>
             </div>
         </div>
     </div>
