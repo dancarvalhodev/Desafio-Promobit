f4de2c78b994959c8080ea75f77585a705535c8f
Daniel Carvalho <danielcarvalhodev@gmail.com>
2022-02-11 10:15:07 -0300

    Adicionado evento para colocar mais campos de tags

---
+++ public/JS/tag.js
@@ -0,0 +1,14 @@
+export default function Tag(){
+    $(document).on('click', '.addTag', (event) => {
+        event.preventDefault();
+        $("<div class='col-sm-12 p-2'><input type='text' class='form-control' name='tag[]' id='tag'></div>").appendTo($(".tagFieldForm"));
+    })
+}
+
+
+
+
+
+
+
+
