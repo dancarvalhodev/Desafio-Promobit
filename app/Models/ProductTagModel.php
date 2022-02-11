183242f6b1f89a0b9e42d1030eb33cbed3e65a47
Daniel Carvalho <danielcarvalhodev@gmail.com>
2022-02-11 08:42:07 -0300

    Adicionado models

---
+++ app/Models/ProductTagModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ProductTagModel extends Model
+{
+    protected $table = 'product_tag';
+    protected $allowedFields = ['name'];
+}
